1cbf6c68f01c90ab58c94d428b9486443afc8020
Brenon Tavares <brenonperez1997@gmail.com>
2025-05-25 21:09:48 -0300

    feat: initial project setup
    - Create Spring Boot project structure
    - Configure basic dependencies
    - Setup project architecture layers
    - Add initial configuration

---
+++ src/main/java/com/westflow/seeds_manager_api/SeedsManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.westflow.seeds_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedsManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedsManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/westflow/seeds_manager_api/SeedsManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westflow.seeds_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedsManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
